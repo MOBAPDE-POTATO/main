2ad784d43232f680b5d660956e9d32250f80c87e
Miko Garcia <crymehonions@gmail.com>
2016-03-08 01:41:33 +0800

    Implemented Log-In and Integration with Web Server
    Log in functionality is now able to get credentials from the web server.
    
    No server error checking (i.e.: wrong password, wrong email, does not
    exist)

--- java/com/example/mikogarcia/findit/LoginActivity.java
+++ java/com/example/mikogarcia/findit/LoginActivity.java
@@ -2,10 +2,12 @@ package com.example.mikogarcia.findit;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -14,12 +16,25 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.mikogarcia.findit.model.Account;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
 /**
  * A login screen that offers login via email/password.
  */
 public class LoginActivity extends AppCompatActivity {
 
     public static final int PASSWORD_LENGTH = 6;
+    public static final String LOGIN_URL = "/FindIT-Web-Service/login.php";
+    public static final String SERVER_IP = "http://192.168.1.150";
 
     // UI references.
     private EditText et_email;
@@ -27,7 +42,6 @@ public class LoginActivity extends AppCompatActivity {
     private TextView createAccountView;
     private View mProgressView;
     private View mLoginFormView;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,18 +93,17 @@ public class LoginActivity extends AppCompatActivity {
         login();
     }
 
+    /**
+     *  Assuming that the credentials are all valid
+     */
     public void login() {
         String email = et_email.getText().toString();
         String pass = et_pass.getText().toString();
 
-        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(getBaseContext()).edit();
-        editor.putString(MainActivity.SP_KEY_USERNAME, et_email.getText().toString());
-        editor.commit();
+        new LogInHelper().execute("crymehonions@gmail.com", "1234");
 
-        setResult(RESULT_OK);
         finish();
     }
-
 
     private boolean isEmailValid(String email) {
         //TODO: Replace this with your own logic
@@ -100,6 +113,46 @@ public class LoginActivity extends AppCompatActivity {
     private boolean isPasswordValid(String password) {
         //TODO: Replace this with your own logic
         return password.length() > PASSWORD_LENGTH;
+    }
+
+    class LogInHelper extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            String email = params[0];
+            String pass = params[1];
+
+            try {
+                Document doc = Jsoup.connect(SERVER_IP+LOGIN_URL)
+                        .data(Account.COLUMN_EMAIL, email)
+                        .data(Account.COLUMN_PASSWORD, pass)
+                        .post();
+
+                Log.i("HTML", doc.toString());
+                Elements elements = doc.select("body");
+
+                String result = "";
+                for (Element e: elements) {
+                    result += e.text();
+                }
+
+                return result;
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Log.i("LOGIN:", s);
+            SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(getBaseContext()).edit();
+            editor.putString(MainActivity.SP_ACCOUNT_JSON_KEY, s);
+            editor.commit();
+        }
     }
 }
 
