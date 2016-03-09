613842cb36096ee0cca830b1281e46211b56ea4a
MartinDLSU <martinlucasrr@gmail.com>
2016-03-09 18:13:07 +0800

    Revert "Just changed IP"
    This reverts commit 0711c28ff04c284860d3268ad6dd8511f8a18644.

--- java/com/example/mikogarcia/findit/RegisterActivity.java
+++ java/com/example/mikogarcia/findit/RegisterActivity.java
@@ -17,9 +17,6 @@ import com.example.mikogarcia.findit.model.Account;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -70,99 +67,12 @@ public class RegisterActivity extends AppCompatActivity {
         String lname = etLastName.getText().toString();
         String email = etEmail.getText().toString();
         String pass = etPassword.getText().toString();
-        String confirmPass = etConfirmPassword.getText().toString();
-        boolean checkfname;
-        boolean checklname;
-        boolean checkEmail;
-        boolean checkPassword;
-        boolean checkConfirmPassword;
-        if(!isEmpty(fname)){
-            checkfname = true;
-        }else{
-            etFirstName.setError("Field must be filled");
-            checkfname = false;
-        }
-        if(!isEmpty(lname)){
-            checklname = true;
-        }else{
-            etLastName.setError("Field must be filled");
-            checklname = false;
-        }
-        if(isEmpty(email)){
-            etEmail.setError("Field must be filled");
-            checkEmail = false;
-        }else {
-            if (!isValidEmail(email)) {
-                etEmail.setError("Invalid Email");
-                checkEmail = false;
-            } else {
-                checkEmail = true;
-            }
-        }
-        if (!isPassLength(pass)) {
-            etPassword.setError("Password must be at least 6 alphanumeric characters");
-            checkPassword = false;
-        }else{
-            if(!isPassAlpha(pass)){
-                etPassword.setError("Password must only contain alphanumeric characters");
-                checkPassword = false;
-            }else{
-                checkPassword = true;
-            }
-        }
-        if(isEmpty(confirmPass)){
-            etConfirmPassword.setError("Field must be filled");
-            checkConfirmPassword = false;
-        }else {
-            if (!isValidConfirmPassword(pass, confirmPass)) {
-                etConfirmPassword.setError("Password does not match");
-                checkConfirmPassword = false;
-            } else {
-                checkConfirmPassword = true;
-            }
-        }
-        if (checkfname == true && checklname == true && checkEmail == true && checkPassword == true && checkConfirmPassword == true) {
-            register(fname, lname, email, pass);
-        }
+
+        register(fname, lname, email, pass);
     }
 
     public void register(String fname, String lname, String email, String pass) {
         new RegisterHelper().execute(fname, lname, email, pass);
-    }
-    private boolean isEmpty(String name) {
-        if (name.length() > 0) {
-            return false;
-        } else {
-            return true;
-        }
-    }
-
-    private boolean isValidEmail(String email){
-        String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" +
-                "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
-
-        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
-        Matcher matcher = pattern.matcher(email);
-        return matcher.matches();
-    }
-
-    private boolean isPassLength(String pass) {
-        if (pass.length() > 5) {
-            return true;
-        }
-        return false;
-    }
-    private boolean isPassAlpha(String pass){
-        String ALPHANUMERIC = "^[a-zA-Z0-9]*$";
-        Pattern pattern = Pattern.compile(ALPHANUMERIC);
-        Matcher matcher = pattern.matcher(pass);
-        return matcher.matches();
-    }
-    private boolean isValidConfirmPassword(String pass, String confirmPass) {
-        if (pass.equals(confirmPass)) {
-            return true;
-        }
-        return false;
     }
 
     public void checkError(String s) {
