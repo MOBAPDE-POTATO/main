15125ef3c374e46ba3d4ae3fccc3c2d0b0b8dadf
MartinDLSU <martinlucasrr@gmail.com>
2016-03-16 21:48:51 +0800

    Add reports error checks
    Item name, place lost and date fields need to be filled. Date field has
    to match mm-dd-yyyy.
    
    On dialogfragment field needs to be filled to be added. Description
    shouldn't match any current descriptions

--- java/com/example/mikogarcia/findit/AddFeatureDialog.java
+++ java/com/example/mikogarcia/findit/AddFeatureDialog.java
@@ -7,7 +7,12 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+
+import com.example.mikogarcia.findit.model.Feature;
+
+import java.util.ArrayList;
 
 /**
  * Created by Daniel on 3/14/2016.
@@ -25,7 +30,17 @@ public class AddFeatureDialog extends DialogFragment{
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         EditText etFeature = (EditText) view.findViewById(R.id.etFeature);
-                        ((AddReportActivity)getActivity()).onAddFeature(etFeature.getText().toString());
+                        ArrayList<Feature> features = ((AddReportActivity) getActivity()).getFeatures();
+                        boolean checkFeature = true;
+                        for(int i = 0; i < features.size();i++) {
+                            if(etFeature.getText().toString().equals(features.get(i).getFeat())) {
+                                etFeature.setError("Description already exists");
+                                checkFeature = false;
+                            }
+                        }
+                        if(checkFeature == true){
+                            ((AddReportActivity) getActivity()).onAddFeature(etFeature.getText().toString());
+                        }
                     }
                 })
                 .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -36,6 +51,52 @@ public class AddFeatureDialog extends DialogFragment{
                 });
 
         return  alertDialogBuilder.create();
+    }
+    public void onStart() {
+        super.onStart();
+        final AlertDialog d = (AlertDialog)getDialog();
+        if(d != null) {
+            Button positiveButton = (Button) d.getButton(Dialog.BUTTON_POSITIVE);
+            positiveButton.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v) {
+                    EditText etFeature = (EditText) view.findViewById(R.id.etFeature);
+                    ArrayList<Feature> features = ((AddReportActivity) getActivity()).getFeatures();
+                    boolean checkFeature = true;
+                    for(int i = 0; i < features.size();i++) {
+                        if(etFeature.getText().toString().equals(features.get(i).getFeat())) {
+                            etFeature.setError("Description already exists");
+                            checkFeature = false;
+                        }
+                    }
+                    if(isEmpty(etFeature.getText().toString())){
+                        checkFeature = false;
+                        etFeature.setError("Field must be filled");
+                    }
+                    if(checkFeature == true){
+                        ((AddReportActivity) getActivity()).onAddFeature(etFeature.getText().toString());
+                        d.dismiss();
+                    }
+                }
+            });
+            Button negativeButton = (Button)d.getButton(Dialog.BUTTON_NEGATIVE);
+            negativeButton.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v) {
+                        d.dismiss();
+                }
+            });
+        }
+    }
+
+    public boolean isEmpty(String desc){
+        if (desc.length() > 0) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
 
