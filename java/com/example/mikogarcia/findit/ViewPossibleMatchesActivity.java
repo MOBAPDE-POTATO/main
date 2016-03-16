bcd92e8b9ec0b6b23c55de08c367af5a9252b05a
dudegrim <cardanomarcdaniel96@gmail.com>
2016-03-17 01:30:47 +0800

    added view possible matches

--- java/com/example/mikogarcia/findit/ViewPossibleMatchesActivity.java
+++ java/com/example/mikogarcia/findit/ViewPossibleMatchesActivity.java
@@ -2,16 +2,25 @@ package com.example.mikogarcia.findit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
-public class ViewPossibleMatchesActivity extends AppCompatActivity {
-    RecyclerView featuresRecycler;
 
+public class ViewPossibleMatchesActivity extends AppCompatActivity {
+
+    RecyclerView matchesRecycler;
+    ReportAdapter reportAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_possible_matches);
         final int id = getIntent().getExtras().getInt("id");
+
+
+        matchesRecycler = (RecyclerView) findViewById(R.id.matchesRecycler);
+        reportAdapter = new ReportAdapter();
+        matchesRecycler.setAdapter(reportAdapter);
+        matchesRecycler.setLayoutManager(new LinearLayoutManager(getBaseContext()));
 
     }
 }
